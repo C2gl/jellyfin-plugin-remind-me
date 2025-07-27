3ffdf334a3be9ea64c3f0e90f14fe8a48c06d40e
C2gl <97646342+C2gl@users.noreply.github.com>
2025-07-27 12:42:52 +0200

    tentative

--- Jellyfin.Plugin.Template/Configuration/PluginConfiguration.cs
+++ Jellyfin.Plugin.Template/Configuration/PluginConfiguration.cs
@@ -3,22 +3,6 @@ using MediaBrowser.Model.Plugins;
 namespace Jellyfin.Plugin.Template.Configuration;
 
 /// <summary>
-/// The configuration options.
-/// </summary>
-public enum SomeOptions
-{
-    /// <summary>
-    /// Option one.
-    /// </summary>
-    OneOption,
-
-    /// <summary>
-    /// Second option.
-    /// </summary>
-    AnotherOption
-}
-
-/// <summary>
 /// Plugin configuration.
 /// </summary>
 public class PluginConfiguration : BasePluginConfiguration
@@ -28,22 +12,33 @@ public class PluginConfiguration : BasePluginConfiguration
     /// </summary>
     public PluginConfiguration()
     {
-        // set default options here
-        Options = SomeOptions.AnotherOption;
-        TrueFalseSetting = true;
-        AnInteger = 2;
-        AString = "string";
+        // Set default options
+        EnableAutoQueue = true;
+        RequireFullWatchPercentage = 80;
+        OnlyForCollections = false;
+        DelayMinutes = 0;
     }
 
     /// <summary>
-    /// Gets or sets a value indicating whether some true or false setting is enabled..
+    /// Gets or sets a value indicating whether auto-queuing is enabled.
     /// </summary>
-    public bool TrueFalseSetting { get; set; }
+    public bool EnableAutoQueue { get; set; }
 
     /// <summary>
-    /// Gets or sets an integer setting.
+    /// Gets or sets the percentage of movie that must be watched to trigger next movie queuing.
     /// </summary>
-    public int AnInteger { get; set; }
+    public int RequireFullWatchPercentage { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether to only work with items in collections.
+    /// </summary>
+    public bool OnlyForCollections { get; set; }
+
+    /// <summary>
+    /// Gets or sets delay in minutes before adding next movie to continue watching.
+    /// </summary>
+    public int DelayMinutes { get; set; }
+}
 
     /// <summary>
     /// Gets or sets a string setting.
--- Jellyfin.Plugin.Template/Plugin.cs
+++ Jellyfin.Plugin.Template/Plugin.cs
@@ -26,10 +26,10 @@ public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
     }
 
     /// <inheritdoc />
-    public override string Name => "Template";
+    public override string Name => "Next in Series";
 
     /// <inheritdoc />
-    public override Guid Id => Guid.Parse("eb5d7894-8eef-4b36-aa6f-5d124e828ce1");
+    public override Guid Id => Guid.Parse("12345678-9abc-def0-1234-56789abcdef0");
 
     /// <summary>
     /// Gets the current plugin instance.
